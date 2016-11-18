ddf3a62a467b193be343bbd0a8baa48a5ee54fdd
Elena Zarkova <ezarkova@gmail.com>
2016-11-18 16:50:59 +0200

    parser changes

--- utils/html-parser.js
+++ utils/html-parser.js
@@ -31,43 +31,49 @@ module.exports.parseDetailedMovie = (selector, html) => {
     const actorSelector = selector.actorSelector;
 
     const posterLink = $(detailedMovieSelector.posterSelector).attr("src");
+    console.log(posterLink);
     const trailerLink = $(detailedMovieSelector.trailerSelector).attr("href");
+    console.log(trailerLink);
     const title = $(detailedMovieSelector.trailerSelector).html();
+    console.log(title);
+    
     const storyLine = $(detailedMovieSelector.storyLineSelector).html();
+    console.log(storyLine);
+    
     const genres = [];
 
-    console.log(title);
+    // console.log(title);
 
     $(detailedMovieSelector.genresSelector).each((index, genre) => {
         const $genre = $(genre);
+        console.log($genre.html());
 
         genres.push($genre.html());
     });
 
-    const releaseDateContainer = $(detailedMovieSelector.releaseDateSelector).hmtl(); // see what prints
-
+    const releaseDateContainer = $(detailedMovieSelector.releaseDateSelector).html(); // see what prints
+    console.log(releaseDateContainer);
+    
     const actors = [];
 
+    console.log($(detailedMovieSelector.actorSelector));
     $(detailedMovieSelector.actorSelector).each((index, actor) => {
         const $actor = $(actor);
 
-        const actorName = actor.find(actorSelector.actorNameSelector).html();
-
-        actors.push({
-            name: actorName
-        });
+        const actorName = $actor.find(actorSelector.actorNameSelector).html();
+        console.log(actorName);
+        actors.push({ name: actorName });
     });
 
-    const movie = {
-            posterLink: posterLink,
-            trailerLink: trailerLink,
-            title: title,
-            storyLine: storyLine,
-            genres: genres,
-            actors: actors
-        };
-        console.log('here');
-        console.log(movie);
+    const movie = { 
+        posterLink,
+        trailerLink,
+        title,
+        storyLine,
+        genres,
+        actors };
+        //console.log('here');
+        //console.log(movie);
 
     return Promise.resolve()
         .then(() => {
